db6ed7e988295dccd4fd8fab50730924c479d0aa
DaniDuese <info@bitstore.dev>
2023-05-08 19:49:34 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -1142,13 +1142,15 @@ export default ({
   </div>
   <div class="fixed bottom-0 left-0 p-4">
     <a href="https://bitstore.dev" target="_blank" class="text-gray-500 hover:text-gray-600 transition duration-150 ease-in-out">
-      <div class="flex items-center space-x-2 drop-shadow-md">
+      <div class="flex items-center space-x-2 drop-shadow-md group/credits">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" class="text-gray-500 h-4 w-4">
           <path fill-rule="evenodd" d="M2.25 6a3 3 0 013-3h13.5a3 3 0 013 3v12a3 3 0 01-3 3H5.25a3 3 0 01-3-3V6zm3.97.97a.75.75 0 011.06 0l2.25 2.25a.75.75 0 010 1.06l-2.25 2.25a.75.75 0 01-1.06-1.06l1.72-1.72-1.72-1.72a.75.75 0 010-1.06zm4.28 4.28a.75.75 0 000 1.5h3a.75.75 0 000-1.5h-3z" clip-rule="evenodd"/>
         </svg>
-        <transition enter-active-class="transition ease-out duration-100" enter-class="opacity-0 translate-y-1" enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-75" leave-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1">
-          <span class="text-xs font-medium">Creators</span>
-        </transition>
+        <!-- Say creators normally but when hovered expand to "Created by Daniel and Guglielmo" and make it transition -->
+
+        <span class="text-xs md:inline group-hover/credits:hidden">Credits</span>
+        <span class="text-xs collapse md:inline group-hover/credits:visible">Created by Daniel and Guglielmo</span>
+
       </div>
     </a>
   </div>
