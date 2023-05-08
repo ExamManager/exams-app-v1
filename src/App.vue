b3e0f8b92fb7382984ea1e74577faff1def77e50
DaniDuese <info@bitstore.dev>
2023-05-08 19:03:25 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -1146,7 +1146,9 @@ export default ({
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" class="text-gray-500 h-4 w-4">
           <path fill-rule="evenodd" d="M2.25 6a3 3 0 013-3h13.5a3 3 0 013 3v12a3 3 0 01-3 3H5.25a3 3 0 01-3-3V6zm3.97.97a.75.75 0 011.06 0l2.25 2.25a.75.75 0 010 1.06l-2.25 2.25a.75.75 0 01-1.06-1.06l1.72-1.72-1.72-1.72a.75.75 0 010-1.06zm4.28 4.28a.75.75 0 000 1.5h3a.75.75 0 000-1.5h-3z" clip-rule="evenodd"/>
         </svg>
-        <span class="text-xs font-medium">Coded by Daniel and Guglielmo</span>
+        <transition enter-active-class="transition ease-out duration-100" enter-class="opacity-0 translate-y-1" enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-75" leave-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-1">
+          <span class="text-xs font-medium">Creators</span>
+        </transition>
       </div>
     </a>
   </div>
