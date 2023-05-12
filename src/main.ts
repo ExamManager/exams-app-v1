a5097d059ba86065cae869ed3e563b9ae1184c7b
DaniDuese <info@bitstore.dev>
2023-05-12 16:28:50 +0100

    Added Multipage

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './views/index.vue'
-
+import App from './views/App.vue'
+import router from './router'
 import VMask from "@ssibrahimbas/v-mask";
 
 
 
 const app = createApp(App)
-app.use(VMask)
+app.use(VMask);
+app.use(router)
 app.mount('#app')
 
---
+++ src/views/App.vue
@@ -0,0 +1,6 @@
+<template>
+    // banner informaation to open fullscreen...
+    <main>
+      <router-view />
+    </main>
+  </template>
