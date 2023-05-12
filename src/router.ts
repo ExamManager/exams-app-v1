401160e67941e3f0dd4e73bb1827f48319e257fb
DaniDuese <info@bitstore.dev>
2023-05-12 16:28:50 +0100

    Added Multipage

---
+++ src/router.ts
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      component: () => import('./views/Home.vue'),
+    },
+    {
+      path: '/fullscreen',
+      component: () => import('./views/fullscreen.vue'),
+    },
+],
+})
+
+export default router
