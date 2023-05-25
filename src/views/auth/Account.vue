eba989547b87db3f8190d91a63eccc47d23d7473
DaniDuese <info@bitstore.dev>
2023-05-25 12:48:13 +0100

    Fixed accounts page

--- src/views/auth/Account.vue
+++ src/views/auth/Account.vue
@@ -194,13 +194,12 @@ export default {
           <!-- on click change old one to false and the one that was clicked to true -->
           <a v-for="item in subNavigation" :key="item.name" @click="
             subNavigation.forEach((item) => (item.current = false));
-          item.current = true;
-          " :class="[
-  item.current
-    ? 'bg-gray-50 text-orange-600 hover:bg-white'
-    : 'text-gray-900 hover:text-gray-900 hover:bg-gray-50',
-  'group rounded-md px-3 py-2 flex items-center text-sm font-medium'
-]" :aria-current="item.current ? 'page' : undefined">
+          item.current = true; " :class="[
+                      item.current
+                        ? 'bg-gray-50 text-orange-600 hover:bg-white'
+                        : 'text-gray-900 hover:text-gray-900 hover:bg-gray-50',
+                      'group rounded-md px-3 py-2 flex items-center text-sm font-medium'
+                    ]" :aria-current="item.current ? 'page' : undefined">
             <component :is="item.icon" :class="[
               item.current
                 ? 'text-orange-500'
@@ -212,16 +211,80 @@ export default {
         </nav>
       </aside>
 
-      <!-- Payment details -->
-      <!-- Only show if it is the current item -->
+      <!-- Profile -->
       <div class="space-y-6 sm:px-6 lg:col-span-9 lg:px-0" v-if="subNavigation[0].current">
+      <form action="#" method="POST">
+        <div class="shadow sm:overflow-hidden sm:rounded-md">
+          <div class="space-y-6 bg-white py-6 px-4 sm:p-6">
+            <div>
+              <h3 class="text-lg font-medium leading-6 text-gray-900">Profile</h3>
+              <p class="mt-1 text-sm text-gray-500">This information will be displayed publicly so be careful what you share.</p>
+            </div>
+
+            <div class="grid grid-cols-3 gap-6">
+              <div class="col-span-3 sm:col-span-2">
+                <label for="company-website" class="block text-sm font-medium text-gray-700">Username</label>
+                <div class="mt-1 flex rounded-md shadow-sm">
+                  <span class="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 bg-gray-50 px-3 text-gray-500 sm:text-sm">workcation.com/</span>
+                  <input type="text" name="username" id="username" autocomplete="username" class="block w-full min-w-0 flex-grow rounded-none rounded-r-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" />
+                </div>
+              </div>
+
+              <div class="col-span-3">
+                <label for="about" class="block text-sm font-medium text-gray-700">About</label>
+                <div class="mt-1">
+                  <textarea id="about" name="about" rows="3" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" placeholder="you@example.com" />
+                </div>
+                <p class="mt-2 text-sm text-gray-500">Brief description for your profile. URLs are hyperlinked.</p>
+              </div>
+
+              <div class="col-span-3">
+                <label class="block text-sm font-medium text-gray-700">Photo</label>
+                <div class="mt-1 flex items-center">
+                  <span class="inline-block h-12 w-12 overflow-hidden rounded-full bg-gray-100">
+                    <svg class="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
+                      <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                    </svg>
+                  </span>
+                  <button type="button" class="ml-5 rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Change</button>
+                </div>
+              </div>
+
+              <div class="col-span-3">
+                <label class="block text-sm font-medium text-gray-700">Cover photo</label>
+                <div class="mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
+                  <div class="space-y-1 text-center">
+                    <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+                      <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                    <div class="flex text-sm text-gray-600">
+                      <label for="file-upload" class="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500">
+                        <span>Upload a file</span>
+                        <input id="file-upload" name="file-upload" type="file" class="sr-only" />
+                      </label>
+                      <p class="pl-1">or drag and drop</p>
+                    </div>
+                    <p class="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="bg-gray-50 px-4 py-3 text-right sm:px-6">
+            <button type="submit" class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Save</button>
+          </div>
+        </div>
+      </form>
+      </div>
+      <!-- Account -->
+      <div class="space-y-6 sm:px-6 lg:col-span-9 lg:px-0" v-if="subNavigation[1].current">
         <section aria-labelledby="payment-details-heading">
           <form action="#" method="POST">
             <div class="shadow sm:overflow-hidden sm:rounded-md">
               <div class="bg-white py-6 px-4 sm:p-6">
                 <div>
                   <h2 id="payment-details-heading" class="text-lg font-medium leading-6 text-gray-900">
-                    Profile Information
+                    Account Information
                   </h2>
                   <p class="mt-1 text-sm text-gray-500">
                     This is the current information for your school. If anything
@@ -551,14 +614,15 @@ export default {
           </form>
         </section>
       </div>
-      <div class="space-y-6 sm:px-6 lg:col-span-9 lg:px-0" v-if="subNavigation[1].current">
+      <!-- Password -->
+      <div class="space-y-6 sm:px-6 lg:col-span-9 lg:px-0" v-if="subNavigation[2].current">
         <section aria-labelledby="payment-details-heading">
           <form action="#" method="POST">
             <div class="shadow sm:overflow-hidden sm:rounded-md">
               <div class="bg-white py-6 px-4 sm:p-6">
                 <div>
                   <h2 id="payment-details-heading" class="text-lg font-medium leading-6 text-gray-900">
-                    Account Information
+                    Password Information
                   </h2>
                   <p class="mt-1 text-sm text-gray-500">
                     This is the current information for your school. If anything
@@ -888,14 +952,15 @@ export default {
           </form>
         </section>
       </div>
-      <div class="space-y-6 sm:px-6 lg:col-span-9 lg:px-0" v-if="subNavigation[2].current">
+      <!-- Notifications -->
+      <div class="space-y-6 sm:px-6 lg:col-span-9 lg:px-0" v-if="subNavigation[3].current">
         <section aria-labelledby="payment-details-heading">
           <form action="#" method="POST">
             <div class="shadow sm:overflow-hidden sm:rounded-md">
               <div class="bg-white py-6 px-4 sm:p-6">
                 <div>
                   <h2 id="payment-details-heading" class="text-lg font-medium leading-6 text-gray-900">
-                    Password Information
+                    Notification Information
                   </h2>
                   <p class="mt-1 text-sm text-gray-500">
                     This is the current information for your school. If anything
