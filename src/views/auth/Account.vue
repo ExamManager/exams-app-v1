1f5dffc69fbeb4d9f4e2970be7ed6d8b623bbd00
DaniEnsi <daniel@ensminger.de>
2023-05-31 19:27:30 +0200

    fixes

--- src/views/auth/Account.vue
+++ src/views/auth/Account.vue
@@ -325,7 +325,7 @@ export default {
                         class="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-gray-900 focus:outline-none focus:ring-gray-900 sm:text-sm" />
                     </div>   
 
-                    <div v-if: class="col-span-4 sm:col-span-2">
+                    <div class="col-span-4 sm:col-span-2">
                       <label for="first-name" class="block text-sm font-medium text-gray-700">{{this.userMetadata.isEnt ? "School Name" : "First Name"}}</label>
                       <input v-model="this.userMetadata.schoolName" readonly type="text" name="school-name" id="school-name"
                         class="mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-gray-900 focus:outline-none focus:ring-gray-900 sm:text-sm" />
