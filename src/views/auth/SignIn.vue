0712d4d1be2f986acc4575198003740b77d1d40e
DaniEnsi <daniel@ensminger.de>
2023-06-12 13:02:05 +0200

    fixes

--- src/views/auth/SignIn.vue
+++ src/views/auth/SignIn.vue
@@ -34,7 +34,7 @@ export default {
 <template>
   <div class="flex min-h-full flex-col justify-center py-20 sm:px-6 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-md">
-      <img class="mx-auto h-12 w-auto" src="src/assets/vue.svg" alt="Your Company" />
+      <img class="mx-auto h-12 w-auto" src="../assets/logo.png" alt="Workflow" />
       <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Login to your account</h2>
       <p class="mt-2 text-center text-sm text-gray-600">
         Or
--- src/views/auth/SignUp.vue
+++ src/views/auth/SignUp.vue
@@ -35,7 +35,7 @@ export default {
 <template>
     <div class="flex min-h-full flex-col justify-center py-20 sm:px-6 lg:px-8">
       <div class="sm:mx-auto sm:w-full sm:max-w-md">
-        <img class="mx-auto h-12 w-auto" src="src/assets/vue.svg" alt="Your Company" />
+        <img class="mx-auto h-12 w-auto" src="../assets/vue.svg" alt="Workflow" />
         <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Create an account</h2>
         <p class="mt-2 text-center text-sm text-gray-600">
           Or
