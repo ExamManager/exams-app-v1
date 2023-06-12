bfc73700afe2ec3361291f77235738064762f385
DaniEnsi <daniel@ensminger.de>
2023-06-12 14:45:25 +0200

    fixes

--- src/router.ts
+++ src/router.ts
@@ -7,26 +7,32 @@ const router = createRouter({
   routes: [
     {
       path: '/',
+      publicPath: '/',
       component: () => import('./views/Home.vue'),
     },
     {
       path: '/free',
+      publicPath: '/free',
       component: () => import('./views/free/Home.vue'),
     },
     {
       path: '/premium',
+      publicPath: '/premium',
       component: () => import('./views/premium/Home.vue'),
     },
     {
       path: '/premium/studentview',
+      publicPath: '/premium/studentview',
       component: () => import('./views/premium/Fullscreen.vue'),
     },
     {
       path: '/pricing',
+      publicPath: '/pricing',
       component: () => import('./views/Pricing.vue'),
     },
     {
       path: '/login',
+      publicPath: '/login',
       component: () => import('./views/auth/SignIn.vue'),
       beforeEnter: async (to, from, next) => {
         const isAuthenticated = await authenticate.methods.checkStatus();
@@ -41,6 +47,7 @@ const router = createRouter({
     },
     {
       path: '/register',
+      publicPath: '/register',
       component: () => import('./views/auth/SignUp.vue'),
       beforeEnter: async (to, from, next) => {
         const isAuthenticated = await authenticate.methods.checkStatus();
@@ -55,10 +62,12 @@ const router = createRouter({
     },
     {
       path: '/:pathMatch(.*)*',
+      publicPath: '/:pathMatch(.*)*',
       component: () => import('./views/404.vue'),
     },
     {
       path: '/account',
+      publicPath: '/account',
       component: () => import('./views/auth/Account.vue'),
       // check if user is authenticated
       beforeEnter: async (to, from, next) => {
@@ -74,6 +83,7 @@ const router = createRouter({
     },
     {
       path: '/setup',
+      publicPath: '/setup',
       component: () => import('./views/auth/AccountSetup.vue'),
       beforeEnter: async (to, from, next) => {
         const isAuthenticated = await authenticate.methods.checkStatus();
